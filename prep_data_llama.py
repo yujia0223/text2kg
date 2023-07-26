cdc4ddb270029b0a4917de7f70630dd6eb329dd3
Tyler <tsadler@ualberta.ca>
2023-07-26 09:54:56 -0600

    Separated out data prep files by prompt.

---
+++ prep_data_llama.py
@@ -0,0 +1,34 @@
+import fire
+from datasets import load_dataset
+import pandas as pd
+
+def main(
+    data_path: str = "",
+    val_set_size: int = 0
+):
+    prompt = "Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.\n\n### Instruction:\n{instruction}\n\n### Input:\n{input}\n\n### Response:\n"
+    def generate_prompt(data_point):
+        res = prompt.format(instruction=data_point["instruction"], input=data_point["input"])
+        res = f"{res}{data_point['output']}"
+        return res
+
+    data = load_dataset(data_path)
+
+    train_val = data["train"].train_test_split(
+        test_size=val_set_size, shuffle=True, seed=42
+    )
+    train_data = []
+    for data in train_val['train'].shuffle():
+        train_data.append(generate_prompt(data))
+    val_data = []
+    for data in train_val['test'].shuffle():
+        val_data.append(generate_prompt(data))
+    train_data = pd.DataFrame(train_data, columns=['text'])
+    val_data = pd.DataFrame(val_data, columns=['text'])
+    print(train_data['text'][0])
+    print(val_data['text'][0])
+    train_data.to_csv("train.csv", index=False)
+    val_data.to_csv("val.csv", index=False)
+
+if __name__ == "__main__":
+    fire.Fire(main)
