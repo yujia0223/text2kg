9e5879e17279d6a7bf788d54aafd81a14993f710
Tyler Sadler <tsadler@ualberta.ca>
2023-11-17 14:37:35 -0700

    Eval results now prints summed scores.

--- get_eval_results.py
+++ get_eval_results.py
@@ -13,9 +13,14 @@ def main(
     data = load_json(file)
     keys_scores = ['Ent_type', 'Partial', 'Exact', 'Strict']
     keys_metrics = ['Precision', 'Recall', 'F1']
+    items = ['Correct', 'Incorrect', 'Partial', 'Missed', 'Spurious', 'Actual', 'Possible']
     for key in keys_scores:
         print(f"For {key}:\nPrecision: {data['Total_scores'][key][keys_metrics[0]]}     Recall: {data['Total_scores'][key][keys_metrics[1]]}     F1: {data['Total_scores'][key][keys_metrics[2]]}\n")
-
+    for key in keys_scores:
+        print(f"For {key}:")
+        for item in items:
+            print(f"{item}: {data['Total_scores'][key][item]}", end=" ")
+        print()
 
 if __name__ == "__main__":
     fire.Fire(main)
