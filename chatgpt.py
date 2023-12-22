aeb47b240d17ec2ab44c9a5bdca38687e0e7c459
Tyler Sadler <tsadler@ualberta.ca>
2023-12-22 15:55:46 -0700

    Commented out API calls

--- chatgpt.py
+++ chatgpt.py
@@ -1,10 +1,11 @@
 import UnlimitedGPT as gpt
 
-session_key = "eyJhbGciOiJkaXIiLCJlbmMiOiJBMjU2R0NNIn0..VbWhkyRN-2f-bRCN.6dcvFqWgkFumVNmjsJKCG2Nv1ycAC5_X0rAYl6Z7JwgV_3BtMURunNK1gciS710oveu16GLBTVcpltUZmqrr9tKQufso3Uv0rCrvLw5Wa6Alr2eZuo9rITLO9gyBuUVc4n7pqcfqWlr6AZ9IhIQm7CloAUUrBYdcIi1kpZSz0Pv4Z_23R2P5wvahJ1iq5QImpd01X5G-7BCUkF7u1mVKMApH9V3Aigqbk0Z8QcgRdH5drHIAq4ZCWmV5jjkNFSZpjK78KlWRA0YBV_fYW8HD63gh4FekiX8mt29XzyF6XIYvR0WhafFVDV0RlBIoRvyiNXnG5tlVubiz6e1V4zVUeNF91IjrziDe6SGsDRCFhgT9WlKrUIkrtZruIv6h0k-4b8A1IWRMGAIH6JVvr2TP7Hts-_SypTWlHMYP9LIf2Qz_KabVdh-hxpTtNXNcdrbiQyVA8JPx_IJamOXpmCE2jGfYapG4zcesKxMTe1gjNZBSmNLVFtuJoD64GnRCSBQjqe4Em2xjwAVA6LCmHjzVueh_R2SlPLLVm5Jvg_2k82a-_ga6k9o_t21iS6cl7mLBA-NE-pyE50haiYoLwstcOrQE1Pich7TBToVV24lcUO2O1L8wHnoWuN5T1E6t-rkLJ5_cY1NHFt7lmkzm_J5Q1QF292w_DxAgwIfhGeanoZtawLu26oKJZ_UJWKGFMC2Uk0Auyu0VTx3JA9FjsGRBvUcgQGKhHjuDELONEaMPjI81jtBc4_GBeDApOapXCtL48g7gx6egZcAq8mMg4xW7gfH14zk2nNPpZHi3qaG5yZmeudpRQN1jjSs7u_pOW9SOrafYEaexb_bRYBPlrKTx9egFFpHTqb-2e1IsJLbpmIFDEYySC4NsTjiDgIY9KFZ_IfBV-imHJJcDwXY4IDKJQgoAU8Y8JEYEk7s4MQG0MPWQ_wUBfvTTL9Xot2PsetKwrhbHauANhcy4P2dowNoVAPLU2INrYbEleeiLGl_yUwsnzRShU6NQauOazCquQfGQNqW9UUusZF96-xx9cDz7XMpOMyQj_9w3qI4Wb-PbQdoqMz0adzRpBunSbNHkjEquhCVHuEMEP0-eAF3m-L9uid2rPUK_ZW1tzoQjblce87vPJcJo69yeZ4pN5rvhJQgFjzm1YLr-xr1m0mhhJq5_FrnZFG-aalNJdGHc6fqL22l0SIXnw10Gw34ptaR0Fo4FiAcr5AUhO9mgwAxn3zYkh3Sc6FDCI3eqLoo1B-ij2waH9-OXGW-v8E0pVV6ZxuMqW22HOUNGThiZabqyQGYq-xPGMEBQDrUH8SeeblJsSh_4dEPAF8MAU1H0URZAGi6zS5xMAbcLSaZivmUYMETZu-PjJzcFZHq_i2-GHJnIgttmeZofyBQCc12T2_h-_UPqniiE3G83wkAG8mBVJ13rx_wir-ccpxB9ZHgY7A7gL_tzOV6YvRx7bjd2xIR-GwMGBXODvBhcCqr62DF4KHYG8TW8eDzjscEAmZ_aoQrA4yZlszq-2uxyupvaSl8TyvTkjs4VYjsH-yCdNoZxYcPQKmTQuuLE0LyTwWRisHzK1qNaKCywYROTVIKC8hiUpDtJKgsKJSIoxdlbKKNbbWH7DZXkTH9N6VJABDlPOD5Vl3hdXQ9aqzSuqdGE2AGSYw18KcMheCgwHNUTzTf7qBtLWI-VbAI9pY4cHhahs1JSDG-iHW_C1z8gCDOX4tWDr5e15JYyYe2unA6YS9gmE5dX-tuLuNKAU4RMQ70oT_Tq02Gz8R-KAsV2nDzFpxWoQBtGOb_SsxqN7UPBKKZZCtfisSWQmjyMv_GXUvSIOetNETjvD5CpzjlqO8A48Yr3ets_8oy_91MOmtT96lxSBn0dcT2zbS767E1d_9s5YBfy85mL5DKCYV53CRBiB1zg9CDHi_FbKvg3R5tNWUwplO93pTWEiVPwUeacbEcvwwIBVzjVFO1Nf9URapxi0SOzdlBWfSfNBXZ3dU5XdNCaiUiYt6I50VBXodJ-rk6nqUn3QKllT3czufASDs4he3HS4SgV0hi1RwESaSZEsZhTd5QcYsP7MvlDjdFAmA07CdWUq2eApE7GDdTcpPNN4tz25hVhB9GfSzufM6qAfuYOl03Lpytm1zTmumDtfQ3gqzM7L7OpWh0CsB42JAQ8814Ff_YdST85axTRazfi-F77A33EIHG7vfbKCtbkmCw6uB8_9lnA4YPuosSZJiB9WPPa4bMK-NydgXlBZ-pxOB_UIWHT6Du2_qRBLtSC2fArCUQQe-m7OYPW6JDvwjpDRbCOTMisyAtz8AOC30e2WIxYLa3VkIFGVs0CS89UJZV2ZSd_jrGqbyUelfki9zqx4baKbjodf43IMxEt36IXLF9esKnMkaQb3BwAt3bQgCAAUObZcvqfYVn8wdH_qfQWmvX6QUPRWzuXfc6msC3FT3xWhMC-krvxocmMINIvJoB4KFmSrHHlq5WJmFdI8VMba9eBmrh92EbgLCWSLi6ZLBc30RIT3raFBSKGlQB3yfcGrDxAhwdg2FzjjTXf8HhAv6UOn1F0lmNtQvlDxWXs4QrkBU_gRxpsmeG52ixVqQtskd5uMaX7NM6Tv9s3Sbb6OgiNqrEO3YFue3fILnTkyoSFTu8fIzt4BF0_f5QoNnRXK0elLdk-tDKkGFugKLI7lgBCpzz-9l9EE3le5zpb_6LKI8TnrdvcqQ.NcL9dwGqMgedZ-JozGc57Q"
+print("File left for possible use in future, although OpenAI does not like the use of unoffical APIs, so do not use this over a long period of time.")
+session_key = ""
 conversation_id_gpt4 = 'f7b4b745-5999-432f-9678-069d5208fe81'  # GPT-4 conversation ID
 conversation_id_gpt3 = '869950cd-b272-49b6-aa2a-ace81c17b3ba'  # GPT-3.5 conversation ID
 print("Initializing API.")
-api = gpt.ChatGPT(session_key, conversation_id=conversation_id_gpt3)
+#api = gpt.ChatGPT(session_key, conversation_id=conversation_id_gpt3)
 print("API ready.")
 input_text = "Pretend that you are great at parsing a text and can effectively extract all entities and their relationships from a text. I will give you a text and you extract all the possible triples. First, extract the triples in textual format, then based on the text, link them to DBPedia vocabularies. Please extract all the triples using the DBpedia vocabulary. Instead of using dbpedia:*, please use dbr:*, dbo:*, dbp:*, or dbc:*. \
 The output format is as below Text-Link Triples: \
@@ -33,10 +34,10 @@ Ned Nickerson - Scott Carty \
 Bess Marvin - Jennifer Pratt \
 George Fayne - Patty Pomplun \
 Seamus - Jonah Von Spreekin [3]"
-message = api.send_message(
-    "Please extract triples from the following text, providing output in the form of (subject, predicate, object): The three laws of motion were first stated by Isaac Newton in his Philosophiæ Naturalis Principia Mathematica (Mathematical Principles of Natural Philosophy), originally published in 1687.",
-    input_mode="SLOW",
-    input_delay=0.1,
-    continue_generating=True
-)
-print(message.response)
+# message = api.send_message(
+#     "Please extract triples from the following text, providing output in the form of (subject, predicate, object): The three laws of motion were first stated by Isaac Newton in his Philosophiæ Naturalis Principia Mathematica (Mathematical Principles of Natural Philosophy), originally published in 1687.",
+#     input_mode="SLOW",
+#     input_delay=0.1,
+#     continue_generating=True
+# )
+#print(message.response)
